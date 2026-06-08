10479a1566bd92a863a443f5b935a29cdd727131
Chris <chrispwong23@gmail.com>
2026-06-07 19:09:52 -0700

    Cleaned up docker

--- backend/app/app.py
+++ backend/app/app.py
@@ -2,8 +2,8 @@ import os
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
-from app.routes.songs import song_bp
-from app.routes.auth import auth_bp
+from backend.app.routes.songs import song_bp
+from backend.app.routes.auth import auth_bp
 
 app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = os.environ["JWT_SECRET_KEY"]
@@ -16,7 +16,5 @@ app.register_blueprint(song_bp, url_prefix='/songs')
 app.register_blueprint(auth_bp, url_prefix="/auth")
 
 
-
 if __name__ == '__main__':
     app.run()
-
--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token
-from app.database.db import getDB
-from app.database.models import new_user
+from backend.app.database.models import new_user
+from backend.app.utils.clients import mongo_client
 import bcrypt
 
 auth_bp = Blueprint("auth", __name__)
+
 
 @auth_bp.route("/register", methods=["POST"])
 def register():
@@ -12,15 +13,15 @@ def register():
     email = data.get("email")
     password = data.get("password")
 
-    db = getDB()
+    db = mongo_client()
 
-    if db.users.find_one({ "email": email }):
-        return jsonify({ "error": "Email already exists" }), 400
+    if db.users.find_one({"email": email}):
+        return jsonify({"error": "Email already exists"}), 400
 
     user = new_user(email, password)
     db.users.insert_one(user)
 
-    return jsonify({ "message": "Account created successfully" }), 201
+    return jsonify({"message": "Account created successfully"}), 201
 
 
 @auth_bp.route("/login", methods=["POST"])
@@ -29,14 +30,14 @@ def login():
     email = data.get("email")
     password = data.get("password")
 
-    db = getDB()
-    user = db.users.find_one({ "email": email })
+    db = mongo_client()
+    user = db.users.find_one({"email": email})
 
     if not user:
-        return jsonify({ "error": "Invalid email or password" }), 401
+        return jsonify({"error": "Invalid email or password"}), 401
 
     if not bcrypt.checkpw(password.encode("utf-8"), user["password"]):
-        return jsonify({ "error": "Invalid email or password" }), 401
+        return jsonify({"error": "Invalid email or password"}), 401
 
     token = create_access_token(identity=str(user["_id"]))
-    return jsonify({ "token": token }), 200
+    return jsonify({"token": token}), 200
