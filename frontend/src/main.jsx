b4f71108ceb30cee6b6c193d257816ebe29eddff
kylelin23 <klin78@calpoly.edu>
2026-06-21 03:13:53 -0700

    update ui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import Home from "./Home.jsx";
-import UploadNew from "./uploadNew.jsx";
-import UploadOld from "./uploadOld.jsx";
 
 
 createRoot(document.getElementById("root")).render(
@@ -13,8 +11,6 @@ createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/uploadNew" element={<UploadNew />} />
-        <Route path="/uploadOld" element={<UploadOld />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
